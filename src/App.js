50e30de01447b76cee6c5e67ce114e2de8917333
Taylor <tbnelson01@gmail.com>
2022-11-15 22:11:17 -0500

    initial header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import 'bulma/css/bulma.min.css'
 
 function App() {
   return (
-    <div className="App">
-      TEST
-    </div>
+    <header className='hero is-small is-info'>
+      <h1 className='hero-body title is-1 has-text-centered'>Corny Cards
+        <button className='button is-pulled-right is-danger'>Create Card</button>
+      </h1>
+          <div className='buttons has-addons'>
+            <button className='button is-white is-outlined'>All cards</button>
+            <button className='button is-white is-outlined'>My cards</button>
+            <button className='button is-white is-outlined'>Friends</button>
+            <button className='button is-white is-outlined'>Account</button>
+        </div>
+    </header >
   );
 }
 
