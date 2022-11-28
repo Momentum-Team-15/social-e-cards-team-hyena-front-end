5634abe230322b8a5bca9770cc7923d03c82ba0a
Corey <coreydblevins@gmail.com>
2022-11-28 11:43:15 -0500

    my cards

--- src/components/MyCards.js
+++ src/components/MyCards.js
@@ -1,7 +1,27 @@
-export const MyCards = () => {
+import { Card } from './Card'
+import { Favorite } from './Favorite'
+
+export const MyCards = (props) => {
     return(
         <div>
-            <h1>My Cards</h1>
+            <h1 className="page-title title is-4 has-text-centered">My Cards</h1>
+            <div className="card-grid">
+            {props.data.map((card, idx) => (
+                <div key={idx}>
+                {props.username === card.user.author && (
+                <div className="card" key={idx}>
+                
+                        <Card card={card}/>
+                        <div className="cardlist">
+                            <p>by {card.user.author}</p>
+                            <Favorite />
+                        </div>
+                    </div>
+                        )}
+                </div>
+                ))}
+
+            </div>
         </div>
     )
 }
