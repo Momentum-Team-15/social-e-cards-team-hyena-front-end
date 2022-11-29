3f095d4c340a4039fa9878da42a7db4c90d8748b
Taylor <tbnelson01@gmail.com>
2022-11-29 15:51:18 -0500

    showing my cards using api

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,24 +4,17 @@ export const Card = (props) => {
         <div className="flip-card">
             <div className="flipping-card">
                 <div className="card-front" style={{
-                    borderWidth: props.card.style.border_width,
-                    borderStyle: props.card.style.border_style,
-                    borderColor: props.card.style.border_color,
-                    backgroundColor: props.card.style.background,
-                    fontSize: props.card.style.font_size
+                    border: `${props.border_style} ${props.border_color}`,
+                    backgroundColor: props.background_color,
                 }}>
-                    <p className="title is-4" style={{
-                        color: props.card.style.text_color
-                    }}>Corny Cards</p>
-                    <p style={{color:props.card.style.text_color}}>{props.card.info.text_front}</p>
+                    <p className="title is-4">Corny Cards</p>
+                    <p>{props.inner_msg}</p>
                 </div>
                 <div className="card-back" style={{
-                    backgroundColor: props.card.style.background,
-                    color: props.card.style.text_color,
-                    border: `${props.card.style.border_width} ${props.card.style.border_style} ${props.card.style.border_color}`
+                    backgroundColor: props.background_color,
+                    border: `${props.border_style} ${props.border_color}`
                 }}>
-                    <p style={{fontSize: props.card.style.font_size}}
-                    >{props.card.info.text_back}</p>
+                    <p>{props.outer_msg}</p>
                 </div>
             </div>
         </div>
