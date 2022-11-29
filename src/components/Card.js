6aebf70ef1c83d776f64b7cb3591cc90613689f8
Taylor <tbnelson01@gmail.com>
2022-11-29 16:19:25 -0500

    css showing for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,20 @@
 export const Card = (props) => {
 
+
     return (
         <div className="flip-card">
             <div className="flipping-card">
                 <div className="card-front" style={{
-                    border: `${props.border_style} ${props.border_color}`,
-                    backgroundColor: props.background_color,
+                    border: `${props.card.border_style} ${props.card.border_color}`,
+                    backgroundColor: props.card.background_color,
                 }}>
-                    <p className="title is-4">Corny Cards</p>
-                    <p>{props.inner_msg}</p>
+                    <p>{props.card.inner_msg}</p>
                 </div>
                 <div className="card-back" style={{
-                    backgroundColor: props.background_color,
-                    border: `${props.border_style} ${props.border_color}`
+                    backgroundColor: props.card.background_color,
+                    border: `${props.card.border_style} ${props.card.border_color}`
                 }}>
-                    <p>{props.outer_msg}</p>
+                    <p>{props.card.outer_msg}</p>
                 </div>
             </div>
         </div>
