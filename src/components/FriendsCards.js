cadd84030355b64ef635dd8ef6369564a6231278
Taylor <tbnelson01@gmail.com>
2022-11-29 18:36:54 -0500

    showing my one friend on page

--- src/components/FriendsCards.js
+++ src/components/FriendsCards.js
@@ -1,7 +1,31 @@
-export const Friends = () => {
-    return (
-        <div>
-            <h1>Friends Page</h1>
+import axios from "axios"
+import { useEffect, useState } from 'react'
+
+
+
+export const Friends = ({ token }) => {
+    const [friendList, setFriendList] = useState([])
+
+    useEffect(() => {
+        axios.get('https://ecard-web-service.onrender.com/friends/', {
+            headers: {
+                Authorization: `Token ${token}`,
+            }
+        })
+            .then(res => setFriendList(res.data))
+    }, [token])
+
+    return (<div>
+        <h1 className="page-title title is-4 has-text-centered">Friends</h1>
+        <div className="card-grid">
+            <div>{friendList.map((friend, idx) => (
+                <div key={idx}>
+                    <div className="card">
+                        <p>{friend.friend}</p>
+                    </div>
+                </div>
+            ))}</div>
         </div>
+    </div>
     )
 }
