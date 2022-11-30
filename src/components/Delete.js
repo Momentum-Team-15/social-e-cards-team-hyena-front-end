80f9c8d8ee17385f96dec1d6f6319692cce397f0
Taylor <tbnelson01@gmail.com>
2022-11-30 17:01:33 -0500

    delete functioning, not refreshing

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -1,9 +1,13 @@
 import { requestDeleteCard } from "./Requests"
 
-
 export const Delete = ({ token, cardId }) => {
 
+
+    const deleteCard = () => {
+        requestDeleteCard(token, cardId)
+    }
+
     return (
-        <button onClick={() => requestDeleteCard(token, cardId)} className="button is-small is-danger">DELETE</button>
+        <button onClick={deleteCard} className="button is-small is-danger">DELETE</button>
     )
 }
