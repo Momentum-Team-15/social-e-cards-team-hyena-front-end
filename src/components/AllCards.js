f2f6ae9c6160de97d132484f5f8f7b7286bb2f4c
Taylor <tbnelson01@gmail.com>
2022-11-30 12:45:30 -0500

    progress

--- src/components/AllCards.js
+++ src/components/AllCards.js
@@ -3,8 +3,10 @@ import { Card } from './Card'
 import { Favorite } from './Favorite';
 import { requestAllCards } from './Requests';
 
-export const AllCards = ({token}) => {
+export const AllCards = ({ token }) => {
     const [cards, setCards] = useState([])
+    const [modal, setModal] = useState(false)
+    const [active, setActive] = useState('')
 
     useEffect(() => {
         requestAllCards(token)
@@ -14,10 +16,18 @@ export const AllCards = ({token}) => {
     return (
         <section>
             <h1 className="page-title title is-4 has-text-centered">All Cards</h1>
+            <button  onClick={() => {active === '' ? (setActive("is-active")) : (setActive(""))}}>modal</button>
+                <div className={`modal ${active}`}>
+                <div className="modal-background"></div>
+                <div className="modal-content">
+                  <p>TESTING</p>
+                </div>
+                <button className="modal-close is-large" aria-label="close"></button>
+              </div>
             <div className="card-grid">
                 {cards.map((card, idx) => (
                     <div className="card" key={idx}>
-                        <Card card={card}/>
+                        <Card card={card} />
                         <div className="cardlist">
                             <p>by {card.user}</p>
                             <Favorite />
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,40 @@
+import { useState } from "react"
+
+
 export const Card = (props) => {
+    const [open, setOpen] = useState(null)
+
+    const handleDetail = () => {
+        { open === props.card.id ? setOpen(null) : setOpen(props.card.id) }
+    }
+    console.log(open)
 
 
     return (
-        <div className="flip-card">
+        <div onClick={handleDetail} className="flip-card">
             <div className="flipping-card">
                 <div className="card-front" style={{
                     border: `${props.card.border_style} ${props.card.border_color}`,
                     backgroundColor: props.card.background_color,
                 }}>
-                    <p className="card-sizing" style={{color: props.card.font_color}}>{props.card.outer_msg}</p>
+                    <p className="card-sizing" style={{ color: props.card.font_color }}>{props.card.outer_msg}</p>
                 </div>
                 <div className="card-back" style={{
                     backgroundColor: props.card.background_color,
                     border: `${props.card.border_style} ${props.card.border_color}`
                 }}>
-                    <p style={{color: props.card.font_color}}>{props.card.inner_msg}</p>
+                    <p style={{ color: props.card.font_color }}>{props.card.inner_msg}</p>
                 </div>
             </div>
+            {open && (
+                <div className="modal is-active">
+                    <div className="modal-background"></div>
+                    <div className="modal-content">
+                        <div className="box">TEST WORDS</div>
+                    </div>
+                    <button className="modal-close is-large"></button>
+                </div>
+            )}
         </div>
     )
 }
